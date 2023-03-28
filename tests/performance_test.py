c14d8968af5a3999b4a34ffc1200bbf3081d86f1
MeldSnake <ikeek.33@gmail.com>
2023-03-28 19:36:43 +0200

    Added performance tests using locust.
    Signed-off-by: MeldSnake <ikeek.33@gmail.com>

---
+++ tests/performance_test.py
@@ -0,0 +1,54 @@
+from locust import HttpUser, task
+
+from server import loadClubs, loadCompetitions
+
+
+class PerformanceTest(HttpUser):
+    """
+    locust -f tests/performance_test.py
+    url: http://192.168.1.64:8089/
+    host: http://127.0.0.1:5000
+    """
+
+    club = loadClubs()[0]
+    competition = loadCompetitions()[1]
+
+    def on_start(self):
+        self.client.post(
+            "/showSummary",
+            {
+                "email": self.club["email"],
+            },
+        )
+
+    @task
+    def reconnect(self):
+        self.client.post(
+            "/showSummary",
+            {
+                "email": self.club["email"],
+            },
+        )
+
+    @task
+    def index(self):
+        self.client.get("/")
+
+    @task
+    def book(self):
+        self.client.get("/book/" + self.competition["name"] + "/" + self.club["name"])
+
+    @task
+    def purchase_places(self):
+        self.client.post(
+            "/purchasePlaces",
+            {
+                "club": self.club["name"],
+                "competition": self.competition["name"],
+                "places": "1",
+            },
+        )
+
+    @task
+    def on_stop(self):
+        self.client.get("/logout")
